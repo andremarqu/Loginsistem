df2c667a531d40332f84f2c78d0e7e33901b3eac
andremarqu <141340718+andremarqu@users.noreply.github.com>
2024-04-29 09:49:43 -0300

    Add files via upload

---
+++ src/home.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+function Home() { 
+    return (
+      <div>
+        <h1>Bem Vindo!</h1>
+        <h3>UFC 300</h3>
+        <iframe width="560" height="315" src="https://www.youtube.com/embed/ibOZ_l-JiXQ?si=hup0Nbg7dGDv-hfN" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+      </div>
+    );
+  
+  }
+  
+  export  {Home};
